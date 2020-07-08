cebd0fe9fd6c09bf1a5f2423bec06a72dd0f519a
paslavskyi9roman <paslavsky.roman@gmail.com>
2020-07-08 13:39:30 +0300

    Order page completed

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -30,10 +30,10 @@ class OrderSummary extends Component {
           <strong>Total Price: {this.props.price.toFixed(2)}</strong>
         </p>
         <p>Continue to Checkout?</p>
-        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
+        <Button btnType='Danger' clicked={this.props.purchaseCancelled}>
           CANCEL
         </Button>
-        <Button btnType="Success" clicked={this.props.purchaseContinued}>
+        <Button btnType='Success' clicked={this.props.purchaseContinued}>
           CONTINUE
         </Button>
       </Auxiliary>
